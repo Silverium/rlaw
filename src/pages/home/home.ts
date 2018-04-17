a9c31025816bdfc38f17c70155c731fb82fb5f7a
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2018-04-17 10:15:56 +0200

    added a floating action button

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CompetencesPage } from '../competences/competences';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  goToCompetences() {
+    this.navCtrl.push(CompetencesPage)
+  }
 }
