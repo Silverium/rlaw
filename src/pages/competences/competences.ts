778fa7df6952de2a1ea523098985851afe7b507f
Soldeplata Saketos Candela <soldeplatadeveloper@gmail.com>
2018-05-15 07:13:05 +0200

    eliiminado subcompetences y reutilizado competences

--- src/pages/competences/competences.ts
+++ src/pages/competences/competences.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { SubcompetencesPage } from '../subcompetences/subcompetences';
 
 /**
  * Generated class for the CompetencesPage page.
@@ -15,90 +14,96 @@ import { SubcompetencesPage } from '../subcompetences/subcompetences';
   templateUrl: 'competences.html',
 })
 export class CompetencesPage {
-  selectedCompetence: Competence;
+  selectedCompetences: Competence;
   competences: Array<Competence>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.selectedCompetence = navParams.get('competence');
-    this.competences = [
-      {
-        title: 'Civil',
-        icon: 'home',
-        subCompetences: [
-          {
-            title: 'Familia',
-            icon: '',
-            description: `Divorcios 
+    this.competences = navParams.get('competences') ||
+      [
+        {
+          title: 'Civil',
+          icon: 'home',
+          competences: [
+            {
+              title: 'Familia',
+              icon: '',
+              description: `Divorcios 
             Modificación de medidas 
             Ejecución de sentencias.
             División de cosa común
             `,
-            subCompetences: [
+              competences: [
 
-            ],
-          },
-          {
-            title: 'Propiedad horizontal',
-            icon: '',
-            description: `Impugnación acuerdos tomados en Junta
+              ],
+            },
+            {
+              title: 'Propiedad horizontal',
+              icon: '',
+              description: `Impugnación acuerdos tomados en Junta
             Reclamación de daños a la comunidad o tercero causante de daños
             Reclamación de cuotas de la comunidad a propietarios deudores
             `,
-           
-          },
-          {
-            title: 'Deshaucios, extinción de contrato',
-            icon: '',
-            description: `Desahucio express por impago de renta.
+
+            },
+            {
+              title: 'Deshaucios, extinción de contrato',
+              icon: '',
+              description: `Desahucio express por impago de renta.
             Resolución de contrato de arrendamiento.
             Reclamación indemnización por daños en la vivienda.
             Reclamación al propietario por daños de mejora.
             `,
 
-          },
-          {
-            title: 'Reclamaciones contractuales o extracontractuales',
-            icon: '',
-            subCompetences: [
-              {  title: 'SEGUROS',
-              icon: '',
-              description: `Reclamación de la cobertura de un siniestro.`,
             },
-              {  title: 'COMPRAVENTA',
+            {
+              title: 'Reclamaciones contractuales o extracontractuales',
               icon: '',
-              description: `Reclamación de arras por duplicado.`,
-            },
-              {  title: 'Deudas',
-              icon: '',
-              description: `Reclamaciones de deudas en general`,
-            },
-              {  title: 'Indemnizaciones',
-              icon: '',
-              description: `Reclamaciones de indemnizaciones por accidentes de tráfico.`,
-            },
-            ],
+              competences: [
+                {
+                  title: 'SEGUROS',
+                  icon: '',
+                  description: `Reclamación de la cobertura de un siniestro.`,
+                },
+                {
+                  title: 'COMPRAVENTA',
+                  icon: '',
+                  description: `Reclamación de arras por duplicado.`,
+                },
+                {
+                  title: 'Deudas',
+                  icon: '',
+                  description: `Reclamaciones de deudas en general`,
+                },
+                {
+                  title: 'Indemnizaciones',
+                  icon: '',
+                  description: `Reclamaciones de indemnizaciones por accidentes de tráfico.`,
+                },
+              ],
 
-          },
-        
-        ],
-      },
-      {
-        title: 'Laboral',
-        icon: 'briefcase',
-        description: 'Conciliación laboral, cálculo indemnización y liquidación',
-      },
-      {
-        title: 'Administrativo',
-        icon: 'paper',
-        description: 'Reclamación impuesto de PLUSVALIA por indebido',
-      },
-    ]
+            },
+
+          ],
+        },
+        {
+          title: 'Laboral',
+          icon: 'briefcase',
+          description: 'Conciliación laboral, cálculo indemnización y liquidación',
+        },
+        {
+          title: 'Administrativo',
+          icon: 'paper',
+          description: 'Reclamación impuesto de PLUSVALIA por indebido',
+        },
+      ]
   }
   competenceTapped(event, competence) {
     // That's right, we're pushing to ourselves!
-    if(competence.subCompetences && competence.subCompetences.length>0)
-    this.navCtrl.push(SubcompetencesPage, {
-      competence
-    });
+    if (competence.competences && competence.competences.length > 0) {
+      console.log(competence)
+      this.navCtrl.push(CompetencesPage, {
+        competences: competence.competences
+      });
+    }
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad CompetencesPage');
@@ -110,12 +115,12 @@ export class Competence {
   title: string;
   description?: string;
   icon: string;
-  subCompetences?: Array<Competence>;
+  competences?: Array<Competence>;
   background?: String;
-  constructor({ title, description, icon, subCompetences, background }) {
+  constructor({ title, description, icon, competences, background }) {
     this.title = title;
     this.description = description;
     this.icon = icon;
-    this.subCompetences = subCompetences;
+    this.competences = competences;
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CompetencesPage } from '../competences/competences';
+import { AboutPage } from '../about/about';
+
 
 @Component({
   selector: 'page-home',
@@ -13,5 +15,14 @@ export class HomePage {
   }
   goToCompetences() {
     this.navCtrl.push(CompetencesPage)
+  }
+  goTo(page) {
+    switch(page){
+      case 'competences':
+      return this.navCtrl.push(CompetencesPage);
+      case 'about':
+      return this.navCtrl.push(AboutPage);
+      
+    }
   }
 }
